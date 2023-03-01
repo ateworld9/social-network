b6a2cd009e9ab2ccfdae0fe090dffba6853c141b
Dmitriy <vahrameev.work@gmail.com>
2023-03-01 21:57:27 +0300

    fix https

--- client/src/7shared/config/constants.ts
+++ client/src/7shared/config/constants.ts
@@ -1,6 +1,6 @@
 // export const API_PREFIX = "http://192.168.0.230:3001";
 export const API_BASE =
   process.env.NODE_ENV === "production"
-    ? "http://ateworld.site"
+    ? "https://ateworld.site"
     : "http://localhost:3001";
 export const API_PREFIX = `${API_BASE}/api`;
